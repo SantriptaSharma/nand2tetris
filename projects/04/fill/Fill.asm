5f844b23f61fbb38cbc1804b851a0f9b5343905a
SantriptaSharma <santripta@gmail.com>
2023-01-17 20:44:08 +0530

    add & complete (04): Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@8192
+D = A
+@SCREEN
+D = D + A
+
+@EOF
+M = D
+
+(LOOP)
+@KBD
+D = M
+@UP
+D; JEQ
+@DOWN
+0; JMP
+
+(UP)
+@SCREEN
+D = A
+
+@i
+M = D
+
+(WHITEN)
+@i
+D = M
+A = D
+M = 0
+
+@i
+MD = M + 1
+
+@EOF
+D = M - D
+
+@WHITEN
+D; JNE
+
+@LOOP
+0; JMP
+
+(DOWN)
+@SCREEN
+D = A
+
+@i
+M = D
+
+(DARKEN)
+@i
+D = M
+A = D
+D = 0
+M = !D
+
+@i
+MD = M + 1
+
+@EOF
+D = M - D
+
+@DARKEN
+D; JNE
+
+@LOOP
+0; JMP
